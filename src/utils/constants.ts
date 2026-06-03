88cb53c7b7e2a9c2aefed489b2a145c04aa8ffb8
blademaster888 <telostia@gmail.com>
2026-06-03 22:01:21 +1000

    fix: hardcode Orbital Market icon as base64 SVG in featuredApps

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -20,6 +20,6 @@ export const featuredApps = [
   {
     name: 'Orbital Market',
     link: 'https://orbitalmarket.trade',
-    icon: 'https://orbitalmarket.trade/favicon.ico',
+    icon: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgd2lkdGg9IjMyIiBoZWlnaHQ9IjMyIj48cmVjdCB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHJ4PSI2IiBmaWxsPSIjMWExYTJlIi8+PHBhdGggZD0iTTEyIDNMMyA3LjVsOSA0LjUgOS00LjVMMTIgM3pNMyAxNi41bDkgNC41IDktNC41TTMgMTJsOSA0LjUgOS00LjUiIHN0cm9rZT0iI2Y1OWUwYiIgc3Ryb2tlLXdpZHRoPSIxLjgiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIgZmlsbD0ibm9uZSIvPjwvc3ZnPg==',
   },
 ];
